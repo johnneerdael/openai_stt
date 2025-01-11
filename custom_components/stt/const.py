c22b3a6d68ca8f8a1bac57868c411c05589bf5e0
John Neerdael <john@neerdael.nl>
2025-01-11 01:55:56 +0100

    Fixed for HA proper integration usage

--- custom_components/stt/const.py
+++ custom_components/stt/const.py
@@ -1,1 +1,12 @@
- 
+"""Constants for the OpenAI STT integration."""
+
+DOMAIN = "stt"
+
+CONF_API_KEY = "api_key"
+CONF_MODEL = "model"
+CONF_PROMPT = "prompt"
+CONF_TEMP = "temperature"
+
+DEFAULT_MODEL = "whisper-1"
+DEFAULT_PROMPT = ""
+DEFAULT_TEMP = 0.0 
